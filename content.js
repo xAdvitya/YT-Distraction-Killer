60c520d81e6b66228042c21a7132d69da242efcc
Advitya <advityasharma21@gmail.com>
2020-04-11 18:44:55 +0530

    first push

---
+++ content.js
@@ -0,0 +1,14 @@
+	//recommendations
+
+	//a = document.getElementById("secondary-inner");
+
+	//a.innserHTML = "<h3>Take control of your day!</h3>"
+
+	//COMPACT recommendations
+
+	x = document.getElementById("related");
+    x.innerHTML = "<h3>Take control of your day!</h3>";
+    
+    	//comments
+	b = document.getElementById("comments");
+	b.remove();
